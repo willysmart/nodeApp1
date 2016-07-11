d1ee874f2a89350101ab14a4f9428beb59248a82
W <W@w.com>
2016-07-10 22:49:09 -0400

    save it

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App 1' });
+  console.log("test 1");
 });
 
 module.exports = router;
