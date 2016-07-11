a49ac058989073482fe1265ce4dfa21878ca0f18
W <W@w.com>
2016-07-11 18:06:00 -0400

    Revert "branch1_add_1"
    This reverts commit dd654d943e208f63cfcf5ad48d9245230de101ce.

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-  console.log("branch1_add_1");
 });
 
 module.exports = router;
