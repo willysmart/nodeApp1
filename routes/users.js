25d83c959513d66c8c0e07a5e404532f2ee89845
W <W@w.com>
2016-07-11 22:30:43 -0400

    save 1

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   console.log("branch1_add_1");
-  console.log("branch2_add_1");
-  console.log("branch2_add_2");
 });
 
 module.exports = router;
